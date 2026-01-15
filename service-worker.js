6b2b3cd820df081e7d13579f41434974e4a400eb
Mike <59297602+zmike174@users.noreply.github.com>
2026-01-15 09:44:18 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'kbju-calculator-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 // Установка Service Worker
